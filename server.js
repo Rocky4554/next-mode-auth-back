b3a511ef1eedf2a1f617aa732ec3b052e00bcfc8
Raunak Kumar <kumarraunak085@gmail.com>
2025-11-09 00:51:37 +0530

    m

--- server.js
+++ server.js
@@ -5,8 +5,28 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const allowedOrigins = [
+  "http://localhost:3000",                  
+  "https://auth-next-app-zfoj.vercel.app" 
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+    
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, 
+
+  })
+);
+
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
